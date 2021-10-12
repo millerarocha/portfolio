6e578d275c34909c631c78c3cbab5519a96db952
MILLER ANDRES ROCHA CASTAÑO <millerarocha@gmail.com>
2021-10-12 13:33:12 -0500

    others project update

--- src/DB/others.js
+++ src/DB/others.js
@@ -3,7 +3,7 @@ const others = [
         id: 0,
         title: "Software SCADA for water meter test benches.",
         image: require('../assets/images/others/elster.JPG'),
-        description: "Software for the automation of water meter test benches, developed usign labview and sql as database.", 
+        description: "Software for the automation of water meter test benches, developed usign labview and sql as database.",
     },
     {
         id: 1,
@@ -17,24 +17,49 @@ const others = [
         title: "Pillypet web app.",
         image: require('../assets/images/others/PILLYPET.png'),
         description: "e-commerce web app developed using bootstrap, angularjs and firebase.", 
+        youtube:"https://www.youtube.com/watch?v=WVeJNblVco8"
     },
     {
         id: 3,
         title: "OIT Machinery alarm supervisor.",
         image: require('../assets/images/others/MachineryIOTAlarms.png'),
-        description: "A web app to monitoring alarms on industrial machinery, developed using bootstrap, angularjs, firebase and arduino esp8266.",  
+        description: "A web app to monitoring alarms on industrial machinery, developed using bootstrap, angularjs, firebase and arduino esp8266.",
+        youtube:"https://www.youtube.com/watch?v=fQGJSYFbhHk"  
     },
     {
         id: 4,
         title: "Automated web app for restaurants.",
         image: require('../assets/images/others/SERVITABLE.png'),
-        description: "A web app to digitalize a restaurant menu, and automate the cooking and service. developed using bootstrap, angularjs and firebase.",  
+        description: "A web app to digitalize a restaurant menu, and automate the cooking and service. developed using bootstrap, angularjs and firebase.",
+        youtube:"https://www.youtube.com/watch?v=Cdj9zD4meE4"  
     },
     {
         id: 5,
-        title: "CO2 Level supervisor.",
+        title: "CO2 Level Monitor.",
         image: require('../assets/images/others/iotQualityAir.png'),
-        description: "A web app to monitoring CO2 levels on eviroment, developed using bootstrap, angularjs, firebase and arduino esp8266.",  
+        description: "A web app to monitoring CO2 levels on eviroment, developed using bootstrap, angularjs, firebase and arduino esp8266.", 
+        youtube:"https://www.youtube.com/watch?v=lAYYOoS5WmU"
+    },
+    {
+        id: 6,
+        title: "Discap web app.",
+        image: require('../assets/images/others/discap.png'),
+        description: "An accessible web app to share info and jobs with persons with visual disabilities.", 
+        youtube:"https://www.youtube.com/watch?v=9qT25uI406o"
+    },
+    {
+        id: 7,
+        title: "Car collision cost calculator app.",
+        image: require('../assets/images/others/licolsim.png'),
+        description: "A web app to calculate the cost of collisions between two cars.", 
+        youtube:"https://www.youtube.com/watch?v=cwAzW41hu9I"
+    },
+    {
+        id: 8,
+        title: "SENA uniforms sales app.",
+        image: require('../assets/images/others/uniforms.png'),
+        description: "A web app for sales of used schools uniforms", 
+        youtube:"https://www.youtube.com/watch?v=mTWpv0dlv5Q"
     },
 ];
 
